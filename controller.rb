a120d78a7b5d530682ede9aa3e8fffb329025361
Gordon Soutter <gordon.soutter@outlook.com>
2020-03-30 20:54:47 +0100

    adds result.erb file to retun string

--- controller.rb
+++ controller.rb
@@ -11,6 +11,7 @@ end
 get '/RPS/:p1/:p2' do
   p1 = params['p1']
   p2 = params['p2']
-  result = RockPaperScissors.run(p1, p2)
-  return result
+  @result = RockPaperScissors.run(p1, p2)
+
+  erb(:result)
 end
